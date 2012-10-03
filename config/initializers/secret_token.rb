0acffa859fbe949f64afea46e77a9ee2d848ba69
Celso de Sa <celsodessa@gmail.com>
2012-10-02 23:31:40 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deslumbre::Application.config.secret_token = '5a0a40e377b647639b6b9eddd1694ca3c89ee137c6bced2577efc34d0dd64abbdc7dc19bd27fed9aa3f5daaa455e1ed2eb611940eaabc4e5ba1df8a302a7ab61'
