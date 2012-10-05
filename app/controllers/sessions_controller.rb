f3ae3b5251a50224c62622a4bfbc23994ff85901
Celso de Sá <730584+CelsoDeSa@users.noreply.github.com>
2012-10-05 01:30:06 -0300

    Try to fix Facebook bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,15 @@ class SessionsController < ApplicationController
   def create
   	user = User.from_omniauth(env["omniauth.auth"])
   	session[:user_id] = user.id
-  	redirect_to root_url
+  	
+	  	respond_to do |format|
+	      format.html { render root_url }
+	      format.json { render root_url }
+	    end
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to root_url
+  	redirect_to('/')
   end
 end
