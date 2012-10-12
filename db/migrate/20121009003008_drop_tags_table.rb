2153aca487811895637c33445e1a2b630b381822
Celso de Sá <730584+CelsoDeSa@users.noreply.github.com>
2012-10-12 00:46:18 -0300

    delete unecessary migrations

--- db/migrate/20121009003008_drop_tags_table.rb
+++
@@ -1,8 +0,0 @@
-class DropTagsTable < ActiveRecord::Migration
-  def up
-  	drop_table :tags
-  end
-
-  def down
-  end
-end
--- db/migrate/20121009004109_drop_taggings_table.rb
+++
@@ -1,8 +0,0 @@
-class DropTaggingsTable < ActiveRecord::Migration
-  def up
-  	drop_table :taggings
-  end
-
-  def down
-  end
-end
