48f076ab3435eeb5a6e7be85d147c14b9dcf5aeb
Celso de Sá <730584+CelsoDeSa@users.noreply.github.com>
2012-10-16 11:25:50 -0300

    Name change

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,6 @@ class User < ActiveRecord::Base
   		user.oauth_expires_at = Time.at(auth.credentials.expires_at)
   		user.save!
   	end
-    @name = user.name
-  end
-
-  def self.name
-    @name  
   end
 
   def facebook
