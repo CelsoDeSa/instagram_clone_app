d3653dc06da6071a53b3f8a5eabd91919cff3352
Celso de Sá <730584+CelsoDeSa@users.noreply.github.com>
2012-10-17 11:49:29 -0300

    get_user_id to Picture.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,10 @@ class Picture < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   acts_as_taggable
   belongs_to :user
+
+  before_save :get_user_id
+
+  def get_user_id
+    user_id: User.find(session[:user_id])
+  end
 end
