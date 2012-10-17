a19c178e6badd58272d0b76c2e06233723c204ab
Celso de Sá <730584+CelsoDeSa@users.noreply.github.com>
2012-10-17 12:07:23 -0300

    correction

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,10 +3,4 @@ class Picture < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   acts_as_taggable
   belongs_to :user
-
-  before_save :get_user_id
-
-  def get_user_id
-    user_id: User.find(session[:user_id])
-  end
 end
